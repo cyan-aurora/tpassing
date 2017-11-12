a701337177971012ceffef36dbf26972f7dc87cc
Cyan Aurora <cyanauroratp@gmail.com>
2017-11-12 11:29:28 -0500

    Add stderr to local cron.log

--- cron.sh
+++ cron.sh
@@ -2,5 +2,6 @@
 
 date >> cron.log
 . venv/bin/activate
-python3 delete-expired.py >> cron.log
-python3 email-updates.py >> cron.log
+python3 delete-expired.py >> cron.log 2>&1
+python3 email-updates.py >> cron.log 2>&1
+
